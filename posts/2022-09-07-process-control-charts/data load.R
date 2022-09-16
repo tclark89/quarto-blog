90d8b85f7cd3f23698c2597fefded969e1b7d224
Tyler Clark <tylerclark2@gmail.com>
2022-09-16 01:51:40 +0000

    Changed from fwf_empty to fwf_widths

--- posts/2022-09-07-process-control-charts/data load.R
+++ posts/2022-09-07-process-control-charts/data load.R
@@ -6,20 +6,20 @@ library(dplyr)
 fileLitho <- read_file("https://www.itl.nist.gov/div898/handbook/datasets/LITHOGRA.DAT")
 # add blog link after publishing?
 
-dataLitho <- read_fwf(fileLitho, 
-         col_positions = fwf_empty(
-           fileLitho, 
-           skip=25,
+dataLitho <- read_fwf(
+  fileLitho, 
+         col_positions = fwf_widths(
+           c(7, 7, 7, 12, 7, 12),
            col_names=c("CASSETTE", 
                        "WAFER", 
                        "SITE", 
                        "LINEWIDT", 
                        "RUNSEQ",
                        "LINEWIDT_2")),
-         skip=25) |> 
+  skip=25
+  ) |> 
   select(-LINEWIDT_2)
 dataLitho
-
 
 # MONITOR-6_3_3_1.DAT ####
 # Counts of defective chips per wafer
